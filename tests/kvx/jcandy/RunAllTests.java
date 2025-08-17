295d06640fec1cf74f1f93a191e5fd11f0d2bcde
karlwizkrafte <karlvince112@gmail.com>
2025-08-17 22:23:19 +0800

    feat: add RunAllTests class to execute all test cases

---
+++ tests/kvx/jcandy/RunAllTests.java
@@ -0,0 +1,9 @@
+package tests.kvx.jcandy;
+
+public class RunAllTests {
+    public static void main(String[] args) {
+        StdTest.run();
+        FlavorfulTest.run();
+        System.out.println("[INFORM] ALL TESTS PASSED");
+    }
+}
